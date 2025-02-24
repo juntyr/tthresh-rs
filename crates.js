0a56b080361937801d9f4a87cddc90141fd0fd04
juntyr <juntyr@users.noreply.github.com>
2025-02-24 08:20:40 +0000

    deploy: 6c2b6a5f93b3e7e0620c37ffd97d23e04ecf54de

---
+++ crates.js
@@ -0,0 +1,2 @@
+window.ALL_CRATES = ["tthresh"];
+//{"start":21,"fragment_lengths":[9]}
---
+++ search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = new Map(JSON.parse('[["tthresh",{"t":"H","n":["tthresh"],"q":[[0,"tthresh"]],"i":"`","f":"{{{d{{f{{d{b}}}}}}}h}","D":"`","p":[[1,"str"],[1,"reference",null,null,1],[1,"slice"],[1,"unit"]],"r":[],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAEAAAAAAAEAEAAAAAAAAQA=","P":[]}]]'));
+if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
+else if (window.initSearch) window.initSearch(searchIndex);
+//{"start":39,"fragment_lengths":[243]}
