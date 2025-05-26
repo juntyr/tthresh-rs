ed1208f9054701dd2e396fe80593de3e109133cf
juntyr <juntyr@users.noreply.github.com>
2025-05-26 16:36:22 +0000

    deploy: f97b7439d8d74cfd5d29d3e7a0d49f22484122fe

---
+++ trait.impl/core/marker/trait.UnsafeUnpin.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["tthresh",[["impl UnsafeUnpin for <a class=\"enum\" href=\"tthresh/enum.Buffer.html\" title=\"enum tthresh::Buffer\">Buffer</a>",1,["tthresh::Buffer"]],["impl UnsafeUnpin for <a class=\"enum\" href=\"tthresh/enum.Error.html\" title=\"enum tthresh::Error\">Error</a>",1,["tthresh::Error"]],["impl UnsafeUnpin for <a class=\"enum\" href=\"tthresh/enum.ErrorBound.html\" title=\"enum tthresh::ErrorBound\">ErrorBound</a>",1,["tthresh::ErrorBound"]]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[448]}
